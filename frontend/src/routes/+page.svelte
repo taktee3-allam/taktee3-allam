be31dfd934145a437c349d32c867b5931ff87ab2
Abdelrahman Abounegm <abounegm@yandex-team.ru>
2024-11-07 00:33:54 +0200

    Add button to store sentences in database

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
 	import { Heading, Button, Textarea, Label, Range, List, Li } from 'flowbite-svelte';
 	import { generateRandomColor } from '$lib/colors';
+	import { supabase } from '$lib/supabase';
+
+	type Sentence = { sentence: string; embedding: number[] };
 
 	let inputText = $state('');
 	let threshold = $state(30);
 	let loading = $state(false);
-	let groupedSentences = $state<string[]>([]);
+	let groupedSentences = $state<Sentence[]>([]);
 
 	async function submit() {
 		loading = true;
 		try {
+			type GroupSentences = { grouped_sentences: Sentence[] };
 			const res = await fetch('http://127.0.0.1:5000/api/split_text', {
 				method: 'POST',
 				headers: { 'Content-Type': 'application/json' },
@@ -18,8 +22,31 @@
 					threshold: threshold / 100
 				})
 			});
-			const data = await res.json();
+			const data: GroupSentences = await res.json();
 			groupedSentences = data.grouped_sentences;
+		} catch (e) {
+			alert('An error occured: ' + e);
+		} finally {
+			loading = false;
+		}
+	}
+
+	async function addToDB() {
+		loading = true;
+		try {
+			const res = await supabase.from('sentences').insert(
+				groupedSentences.map(({ sentence, embedding }) => ({
+					sentence,
+					embedding: JSON.stringify(embedding)
+				}))
+			);
+			if (res.error) {
+				throw 'An error occured: ' + res.error.message;
+			}
+
+			alert('Sentences added to database successfully');
+			inputText = '';
+			groupedSentences = [];
 		} catch (e) {
 			alert('An error occured: ' + e);
 		} finally {
@@ -46,9 +73,12 @@
 	<section class="flex-1">
 		<Heading tag="h2">Grouped sentences</Heading>
 		<List class="p-2">
-			{#each groupedSentences as sentence}
+			{#each groupedSentences as { sentence }}
 				<Li style="color: {generateRandomColor()}">{sentence}</Li>
 			{/each}
 		</List>
+		{#if groupedSentences.length > 0}
+			<Button onclick={addToDB} disabled={loading}>Add to Database</Button>
+		{/if}
 	</section>
 </main>
