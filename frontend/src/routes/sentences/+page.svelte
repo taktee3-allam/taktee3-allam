0d1cda09bbb2f3cac26236f68b05fa538a0c2c87
Abdelrahman Abounegm <abounegm@yandex-team.ru>
2024-11-08 13:20:35 +0200

    Add delete button

--- frontend/src/routes/sentences/+page.svelte
+++ frontend/src/routes/sentences/+page.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 	import {
 		Input,
+		Button,
 		Table,
 		TableBody,
 		TableBodyCell,
@@ -9,6 +10,7 @@
 		TableHead,
 		TableHeadCell
 	} from 'flowbite-svelte';
+	import { TrashBinSolid } from 'flowbite-svelte-icons';
 	import { supabase } from '$lib/supabase';
 	import type { Tables } from '$lib/database.types';
 
@@ -29,23 +31,42 @@
 			groupedSentences = data;
 		}
 	});
+
+	async function deleteSentence(id: number) {
+		const res = confirm('هل انت متأكد؟');
+		if (!res) {
+			return;
+		}
+		const { error } = await supabase.from('sentences').delete().eq('id', id);
+		if (error) {
+			alert('An error occured: ' + error.message);
+		} else {
+			groupedSentences = groupedSentences.filter((s) => s.id !== id);
+		}
+	}
 </script>
 
 <Input placeholder="ابحث في الجمل" bind:value={search} class="w-full" />
 
 <Table striped hoverable>
 	<TableHead>
-		<TableHeadCell class="inline-block w-8/12">الجملة</TableHeadCell>
+		<TableHeadCell class="inline-block w-7/12">الجملة</TableHeadCell>
 		<TableHeadCell class="inline-block w-3/12">Embedding</TableHeadCell>
+		<TableHeadCell class="inline-block w-1/12">امسح</TableHeadCell>
 	</TableHead>
-	<TableBody tableBodyClass="inline-block max-w-[100vw]">
-		{#each filteredSentences as { sentence, embedding }}
+	<TableBody tableBodyClass="inline-block max-w-[95vw]">
+		{#each filteredSentences as { id, sentence, embedding }}
 			<TableBodyRow class="inline-block w-full">
-				<TableBodyCell class="inline-block w-8/12 truncate">
+				<TableBodyCell class="inline-block w-7/12 truncate">
 					{sentence}
 				</TableBodyCell>
 				<TableBodyCell class="inline-block w-3/12 truncate">
 					{embedding}
+				</TableBodyCell>
+				<TableBodyCell class="inline-block w-1/12 truncate">
+					<Button on:click={() => deleteSentence(id)}>
+						<TrashBinSolid />
+					</Button>
 				</TableBodyCell>
 			</TableBodyRow>
 		{/each}
